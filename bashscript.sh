392ca22455f2760a7f0a3febe2ce8d7ddda0d87e
Ubuntu <ubuntu@ip-172-31-29-87.ap-southeast-1.compute.internal>
2023-09-21 04:29:36 +0000

    task completed

---
+++ bashscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Enter your name:'
+read name
+echo "Hello,${name} how are you?"
+read status
+if [ $status ==  "fine" ];then
+	echo "It\'s nice"
+else 
+	echo 'you wil be fine'
+fi
+
